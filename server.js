1ace21ac904be72f7046dc52f179f7161f8b129b
raghd <raghadmohsmadi@hotmail.com>
2022-05-07 21:47:40 +0300

    task12

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 'use strict'
-const express = require('express');
-const app = express(); 
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios").default;
+require("dotenv").config();
 const PORT = 3000;
-const movieData = require('./MovieData/Data.json');
+const app = express();
+app.use(cors());
+let APIKEY =process.env.APIKEY ;
+//console.log(APIKEY)
 
+//const P = process.env.P;
 
+//dotenv.config();
 
 app.listen(PORT, handleListener);
+app.use(cors());
 app.get('/favorite', handleFavorite);
 app.get('/', handleData);
+app.get('/trending', trendingHandler)
+app.get('/search', searchTrendingHandler)
 app.use('/error', (req, res) => res.send(error()));
 app.get('*', handelNotFound);
-
 
 
 
@@ -45,13 +54,49 @@ app.use(function (err, req, res, text) {
     res.send("Sorry something wrong");
 });
 
-
-
-
-
-function Movie(title, poster_path, overview) {
+function Movie(id ,title,release_date, poster_path, overview) {
+    this.id = id;
     this.title = title;
+    this.release_date = release_date;
     this.poster_path = poster_path;
     this.overview = overview;
 
 }
+function trendingHandler(req, res) {
+    let result = [];
+    
+    axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${APIKEY}`)
+    //axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=37ddc7081e348bf246a42f3be2b3dfd0&language=en-US`)
+        .then(apiResponse => {
+            apiResponse.data.results.map(value => {
+                let oneTrend = new Movie(value.id, value.title, value.release_date, value.poster_path, value.overview);
+                result.push(oneTrend);
+            })
+            return res.status(200).json(result);
+        }).catch(error => {
+            res.send("error in getting data from API")
+        })
+    }
+function searchTrendingHandler(req, res) {
+    const search = req.query.data
+    let results = [];
+ console.log(search,APIKEY);
+  //axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&query=${search}`)
+ axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&language=en-US&query=${search}`)
+        .then(apiResponse => {
+           let data=  apiResponse.data 
+          // console.log(data.results);
+           data.results.map(value => {
+                let oneTrend = new Movie(value.id || "N/A", value.title || "N/A", value.release_date || "N/A", value.poster_path || "N/A", value.overview || "N/A")
+                results.push(oneTrend);
+            });
+            return res.status(200).json(results);
+        }).catch(error => {res.send("error in getting data from API")
+           // errorHandler(error, req, res);
+        })
+}
+
+
+
+
+
